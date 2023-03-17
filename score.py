10cecee434c63b7470b7bb5bb48a4577822e5ba9
Adrian Szvoren <svoren.a@gmail.com>
2023-03-17 13:24:38 +0000

    added config, sidebar options for diff platforms and changed the title formatting

--- score.py
+++ score.py
@@ -1,13 +1,15 @@
 import pickle
 
 from sklearn.pipeline import Pipeline
+from typing import List
 
 
 def load_model(model_path: str) -> Pipeline:
     pipe = pickle.load(open(f'{model_path}/pipe.pickle', 'rb'))
     return pipe
 
-def score(text: str) -> float:
+
+def score(text: str) -> List[float]:
     model_path = 'model'
 
     pipe = load_model(model_path)
