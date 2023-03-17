7fa7f08b8793fe7193e02f5568452fad72a9fbfa
Adrian Szvoren <svoren.a@gmail.com>
2023-03-17 15:41:51 +0000

    removed images and improved interface by using emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,48 +15,36 @@ if __name__ == '__main__':
         placeholder='Write your review here.',
         label_visibility='collapsed'
     )
+
     platform = st.selectbox(
-        'Range and platform selection',
+        'Platform selection',
         [
             '',
-            '0.5 - 5 (Letterboxd)',
-            '1 - 10 (IMDB)',
-            '0 - 100% (Rotten Tomatoes)'
+            'Letterboxd (0.5 - 5)',
+            'IMDB (1 - 10)',
+            'Rotten Tomatoes (0 - 100%)'
         ],
         label_visibility='collapsed'
     )
 
     score = score(text_input)[0]
 
-    if platform != '':
-        col1, col2 = st.columns([1, 2])
-        with col1:
-            st.subheader(f'Movie rating:')
-
+    score_verbal = ''
     if 'Letterboxd' in platform:
         score_lb = math.ceil(10 * score)/2
         stars = '★' * math.floor(score_lb)
         if score_lb % 1 == 0.5:
             stars += '½'
-        stars += '☆'*math.floor(5-score_lb)
-        with col2:
-            st.subheader(f'{stars}')
+        stars += '☆' * math.floor(5-score_lb)
+        score_verbal = f'{stars} ({score_lb})'
     elif 'IMDB' in platform:
         score_imdb = math.ceil(10 * score)
-        with col2:
-            st.subheader(f':star: **{score_imdb}**/10')
+        score_verbal = f':star: **{score_imdb}**/10'
     elif 'Rotten Tomatoes' in platform:
         score_rt = round(100 * score)
-        if score_rt >= 75:
-            score_rt_verbal = 'Certified_Fresh'
-        elif score_rt >= 60:
-            score_rt_verbal = 'Fresh'
+        if score_rt >= 60:
+            score_rt_verbal = ':tomato:'
         else:
-            score_rt_verbal = 'Rotten'
-        with col2:
-            col21, col22 = st.columns([1, 7])
-            with col21:
-                st.image(f'img/RT_{score_rt_verbal}.png', width=50)
-            with col22:
-                st.subheader(f'{score_rt}%')
-
+            score_rt_verbal = ':microbe:'
+        score_verbal = f'{score_rt_verbal} {score_rt}%'
+    st.subheader(f'Movie rating: {score_verbal}')
